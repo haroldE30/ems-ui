79783ccb7eff13aec5c7b2e5431f66f505eac820
haroldE30 <hasiasat@yahoo.com>
2020-08-04 16:10:55 +0800

    Update app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hrms-ui'`, () => {
+  it(`should have as title 'ems-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hrms-ui');
+    expect(app.title).toEqual('ems-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('hrms-ui app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ems-ui app is running!');
   });
 });
